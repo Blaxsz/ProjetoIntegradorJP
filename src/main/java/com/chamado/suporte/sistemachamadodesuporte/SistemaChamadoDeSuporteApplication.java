74826e29bfeaee5699682827b8b30f220709394f
Blaxsz <joaovictor.gizidoro@pm.me>
2023-10-10 05:36:44 -0300

    Adicionando arquivos e diretórios padrões.

---
+++ src/main/java/com/chamado/suporte/sistemachamadodesuporte/SistemaChamadoDeSuporteApplication.java
@@ -0,0 +1,13 @@
+package com.chamado.suporte.sistemachamadodesuporte;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaChamadoDeSuporteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SistemaChamadoDeSuporteApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/chamado/suporte/sistemachamadodesuporte/SistemaChamadoDeSuporteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chamado.suporte.sistemachamadodesuporte;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaChamadoDeSuporteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
